705d3cf58684b49cd3416ddb126c3846e81b2b3a
j-alex-larrimore <j.alex.larrimore@gmail.com>
2014-07-28 14:55:16 -0700

    first commit

---
+++ changepassword.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'core/init.php';
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        require_once 'core/init.php';
+        
+//        $user = DB::getInstance()->get('users', array('username', '=', 'alex'));
+//        
+//        if(!$user->count()){
+//            echo 'No user';
+//        }
+//        else {
+//           echo $user->first()->username;
+//        }
+        
+        
+        
+        ?>
+    </body>
+</html>
